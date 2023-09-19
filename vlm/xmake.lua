c87a84177ad360a22da32d4be324a3bfed08e699
Samuel Ayala <samayala22@gmail.com>
2023-09-18 21:56:53 -0400

    added avx2 influence kernel
    but is broken and im commiting so that I can debug on other pc

--- vlm/xmake.lua
+++ vlm/xmake.lua
@@ -2,7 +2,7 @@ add_requires("eigen")
 
 target("vlm")
     set_kind("binary")
-    set_runargs({"-i"}, {"../../../../config/elliptic.vlm"}, {"-m"}, {"../../../../mesh/elliptic_python.xyz"}, {"-o"}, {"../../../../results/elliptic.vtu"})
+    set_runargs({"-i"}, {"../../../../config/elliptic.vlm"}, {"-m"}, {"../../../../mesh/elliptic_64x64.xyz"}, {"-o"}, {"../../../../results/elliptic.vtu"})
     add_packages("eigen")
     -- add_packages("tbb", "lz4")
     add_files("src/*.cpp")
