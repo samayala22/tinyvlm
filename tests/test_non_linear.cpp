280623d7e79024b04848afcf86b89c1fc32239fe
Samuel Ayala <samayala22@gmail.com>
2024-02-01 14:38:19 -0500

    add file output
    output the alpha and cl vectors to files

--- tests/test_non_linear.cpp
+++ tests/test_non_linear.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <cmath>
 #include <cstdio>
+#include <fstream>
+#include <utility>
 
 #include "tinyinterpolate.hpp"
 #include "vlm.hpp"
@@ -43,15 +45,37 @@ f32 to_radians(f32 degrees) {
     return degrees * PI_f / 180.0f;
 }
 
+f32 to_degrees(f32 radians) {
+    return radians * 180.0f / PI_f;
+}
+
+template<typename T>
+void write_vector_pair(const std::string& filename, const std::vector<T>& vec1, const std::vector<T>& vec2) {
+    assert(vec1.size() == vec2.size());
+    std::ofstream outFile(filename + ".dat");
+
+    // Check if file is open
+    if (!outFile.is_open()) throw std::runtime_error("Failed to open file: " + filename);
+
+    const u64 n = vec1.size();
+    outFile << n << '\n';
+    for (u64 i = 0; i < n; i++) {
+        outFile << vec1[i] << ' ' << vec2[i] << '\n';
+    }
+    outFile.close();
+}
+
 int main(int argc, char** argv) {
     const std::vector<std::string> meshes = {"../../../../mesh/infinite_rectangular_5x1000.x"};
     const std::vector<std::string> backends = {"avx2"};
-    std::vector<std::unique_ptr<LiftCurveFunctor>> lift_curves;
-    lift_curves.emplace_back(std::make_unique<SpallartLiftCurve>(1.2f, 0.28f, 0.02f, 2.f*PI_f, 2.f*PI_f));
-    lift_curves.emplace_back(std::make_unique<SpallartLiftCurve>(0.72f, 0.28f, 0.04f, 2.f*PI_f, 1.5f*PI_f));
-    lift_curves.emplace_back(std::make_unique<ThinAirfoilPolarLiftCurve>());
-    std::vector<f32> alphas = {0, 5, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-    //std::transform(alphas.begin(), alphas.end(), alphas.begin(), to_radians);
+    std::vector<std::pair<std::string, std::unique_ptr<LiftCurveFunctor>>> lift_curves;
+    lift_curves.emplace_back(std::make_pair("spallart1", std::make_unique<SpallartLiftCurve>(1.2f, 0.28f, 0.02f, 2.f*PI_f, 2.f*PI_f)));
+    lift_curves.emplace_back(std::make_pair("spallart2", std::make_unique<SpallartLiftCurve>(0.72f, 0.28f, 0.04f, 2.f*PI_f, 1.5f*PI_f)));
+    lift_curves.emplace_back(std::make_pair("polar", std::make_unique<ThinAirfoilPolarLiftCurve>()));
+    
+    std::vector<f32> test_alphas = {0, 5, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+    std::transform(test_alphas.begin(), test_alphas.end(), test_alphas.begin(), to_radians);
+    std::vector<f32> test_cl(test_alphas.size());
 
     std::vector<f32> db_alphas;
     linspace(to_radians(0.f), to_radians(20.f), 100, db_alphas);
@@ -66,7 +90,9 @@ int main(int argc, char** argv) {
         solver.backend = create_backend(backend_name, *solver.mesh);
 
         for (const auto& lift_curve : lift_curves) {
-            std::transform(db_alphas.begin(), db_alphas.end(), db_cl.begin(), [&lift_curve](float alpha){ return (*lift_curve)(alpha); });
+            std::transform(db_alphas.begin(), db_alphas.end(), db_cl.begin(), [&lift_curve](float alpha){ return (*lift_curve.second)(alpha); });
+            write_vector_pair(lift_curve.first + "_analytical_cl", db_alphas, db_cl);
+
             Database db;
             db.profiles.emplace_back(
                 WingProfile<tiny::AkimaInterpolator<f32>>(
@@ -80,16 +106,18 @@ int main(int argc, char** argv) {
             );
             db.profiles_pos.emplace_back(0.0f);
             
-            for (const auto& alpha : alphas) {
-                const FlowData flow{to_radians(alpha), 0.0f, 1.0f, 1.0f};
+            for (u32 i = 0; i < test_alphas.size(); i++) {
+                const FlowData flow{test_alphas[i], 0.0f, 1.0f, 1.0f};
                 auto coeffs = solver.solve(flow, db);
-                std::printf(">>> Alpha: %.1f | CL = %.6f CD = %.6f CMx = %.6f CMy = %.6f CMz = %.6f\n", alpha, coeffs.cl, coeffs.cd, coeffs.cm.x, coeffs.cm.y, coeffs.cm.z);
-                const f32 analytical_cl = (*lift_curve)(flow.alpha);
+                test_cl[i] = coeffs.cl;
+                //std::printf(">>> Alpha: %.1f | CL = %.6f CD = %.6f CMx = %.6f CMy = %.6f CMz = %.6f\n", to_degrees(test_alphas[i]), coeffs.cl, coeffs.cd, coeffs.cm.x, coeffs.cm.y, coeffs.cm.z);
+                const f32 analytical_cl = (*lift_curve.second)(flow.alpha);
                 const f32 abs_error = std::abs(coeffs.cl - analytical_cl);
                 const f32 rel_error = 100.0f * abs_error / (analytical_cl + std::numeric_limits<f32>::epsilon());
-                std::printf(">>> Analytical: %.6f | Abs Error: %.3E | Relative Error: %.5f%% \n", analytical_cl, abs_error, rel_error);
+                //std::printf(">>> Analytical: %.6f | Abs Error: %.3E | Relative Error: %.5f%% \n", analytical_cl, abs_error, rel_error);
                 if (rel_error > 1.f) return 1; // Failure
             }
+            write_vector_pair(lift_curve.first + "_nonlinear_cl", test_alphas, test_cl);
         }
     }
 
