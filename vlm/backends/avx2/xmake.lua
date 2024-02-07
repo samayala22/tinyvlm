a7871c7c5144e0c9fbe8243d978a2bce4ecfa8b9
Samuel Ayala <samayala22@gmail.com>
2024-02-06 22:11:19 -0500

    initial commit with lhs taskflow

--- vlm/backends/avx2/xmake.lua
+++ vlm/backends/avx2/xmake.lua
@@ -1,12 +1,14 @@
 add_requires("tbb")
 
 add_requires("openblas")
+add_requires("taskflow")
 
 target("backend-avx2")
     set_kind("static")
     set_default(false)
     add_vectorexts("avx2", "fma")
     add_packages("tbb")
+    add_packages("taskflow")
     add_defines("HAVE_LAPACK_CONFIG_H")
     add_packages("openblas", { public = true })
 
