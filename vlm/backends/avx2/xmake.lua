d41b7fa423122da91a96f8deb9026a33114fd144
Samuel Ayala <samayala22@gmail.com>
2024-02-08 16:36:46 -0500

    add executor singleton and remove tbb

--- vlm/backends/avx2/xmake.lua
+++ vlm/backends/avx2/xmake.lua
@@ -1,7 +1,3 @@
-includes("../../../packages/taskflow.lua")
-
-add_requires("tbb")
-
 add_requires("openblas")
 add_requires("taskflow_custom")
 
@@ -9,7 +5,6 @@ target("backend-avx2")
     set_kind("static")
     set_default(false)
     add_vectorexts("avx2", "fma")
-    add_packages("tbb")
     add_packages("taskflow_custom")
     add_defines("HAVE_LAPACK_CONFIG_H")
     add_packages("openblas", { public = true })
