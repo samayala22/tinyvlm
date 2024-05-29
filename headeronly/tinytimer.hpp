eeb283e074610e81e68ec2078a8ec5943c057a52
Samuel Ayala <samayala22@gmail.com>
2024-05-28 22:20:09 -0400

    timer use nanos

--- headeronly/tinytimer.hpp
+++ headeronly/tinytimer.hpp
@@ -5,8 +5,9 @@
 
 namespace tiny {
 
-constexpr static double us_in_s = 1'000'000;
-constexpr static double us_in_ms = 1'000;
+constexpr static double ns_in_s = 1'000'000'000;
+constexpr static double ns_in_ms = 1'000'000;
+constexpr static double ns_in_us = 1'000;
 
 class ScopedTimer {
 public:
@@ -14,15 +15,17 @@ public:
 
     ~ScopedTimer() {
         const auto m_end = std::chrono::high_resolution_clock::now();
-        const auto duration = static_cast<double>(std::chrono::duration_cast<std::chrono::microseconds>(m_end - m_start).count());
+        const auto duration = static_cast<double>(std::chrono::duration_cast<std::chrono::nanoseconds>(m_end - m_start).count());
         std::printf("%s: ", m_name);
 
-        if (duration > us_in_s) {
-            std::printf("%.2f s\n", duration / us_in_s);
-        } else if (duration > us_in_ms) {
-            std::printf("%.0f ms\n", duration / us_in_ms);
+        if (duration > ns_in_s) {
+            std::printf("%.2f s\n", duration / ns_in_s);
+        } else if (duration > ns_in_ms) {
+            std::printf("%.2f ms\n", duration / ns_in_ms);
+        } else if (duration > ns_in_us) {
+            std::printf("%.2f us\n", duration / ns_in_us);
         } else {
-            std::printf("%.0f us\n", duration);
+            std::printf("%.0f ns\n", duration);
         }
     }
 private:
@@ -30,4 +33,4 @@ private:
     const std::chrono::time_point<std::chrono::high_resolution_clock> m_start;
 };
 
-}
+} // namespace tiny
