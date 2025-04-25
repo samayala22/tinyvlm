3cb714f9b66f2d83ece54589116f04f8811a6491
Samuel Ayala <samayala22@gmail.com>
2025-04-25 14:14:11 -0400

    random stuff

--- data/dof2_hb.py
+++ data/dof2_hb.py
@@ -2,9 +2,12 @@ import numpy as np
 import scipy as sp
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+import autograd
+
 import os
 import subprocess
 from enum import Enum
+from dataclasses import dataclass
 
 # local imports
 import dof2
@@ -12,11 +15,14 @@ import vanderpol as vdp
 import finite_diff as fd
 import helpers
 
-np.set_printoptions(formatter={'float': '{:.4e}'.format}) # format shortE
+np.set_printoptions(
+    linewidth=200, # max line width
+    formatter={'float': '{:.3e}'.format} # format shortE
+) 
 
 def getenv(key):
     var = os.getenv(key)
-    if int(var) == 0:
+    if not var or int(var) == 0:
         return False
     return True
 
@@ -62,16 +68,35 @@ def run_hbvlm(omega, H, read_gamma, write_gamma, reset_logfile=False):
             print(f"Command failed (return code {result.returncode}), see {logfile_path}")
             exit(1)
 
+@dataclass
+class System:
+    M: np.ndarray
+    C: np.ndarray
+    K: np.ndarray
+    dMdU: np.ndarray
+    dCdU: np.ndarray
+    dKdU: np.ndarray
+    fnlt: callable # function nonlinear time domain input/time domain output
+    dfntldU: callable # function nonlinear time domain derivative
+    fnlf: callable # function nonlinear frequency domain input/time domain output
+    dfnlfdU: callable # function nonlinear frequency domain derivative
+
 def create_motion_system(omega: float, U_param: float, read_gamma: bool=False, write_gamma: bool=False):
     # NLvib params
-    def func_nl(t, u, v):
+    def fnlt(t, u, v):
         return np.array([
             - (ndv.omega / U_param)**2 * u[0],
             - 1/(U_param**2) * torsional_func(u[1])
         ])
     
+    def dfntldU(t, u, v):
+        return np.array([
+            2.0 * (ndv.omega / U_param)**3 * u[0],
+            2.0 /(U_param**3) * torsional_func(u[1])
+        ])
+    
     # @helpers.Timing(prefix="HBVLM: ")
-    def func_nl_freq(X):
+    def fnlf(X):
         np.save("build/windows/x64/release/kin_coeffs.npy", X)
         run_hbvlm(omega, H, read_gamma, write_gamma)
         coeffs = np.load("build/windows/x64/release/hbvlm_t.npy").astype(np.float64)
@@ -80,21 +105,25 @@ def create_motion_system(omega: float, U_param: float, read_gamma: bool=False, w
         # coeffs = np.zeros_like(coeffs)
         return coeffs
     
+    def dfnlfdU(X):
+        return np.zeros(2)
+    
     M = np.array([
         [1.0, ndv.x_a],
         [ndv.x_a / (ndv.r_a**2), 1.0]
     ])
-    C = np.array([
-        [2.0*ndv.zeta_h*(ndv.omega/U_param), 0],
-        [0, 2.0*ndv.zeta_a/U_param]
-    ])
+    dMdU = np.zeros_like(M)
+    C_ = np.array([[2.0 * ndv.zeta_h * ndv.omega, 0], [0, 2.0 * ndv.zeta_a]])
+    C = C_ / U_param
+    dCdU = - C_ / U_param**2
     # K = np.array([
     #     [(ndv.omega/U_param)**2, 0],
     #     [0, 1/(U_param**2)]
     # ])
     K = np.zeros((2,2))
+    dKdU = np.zeros_like(K)
 
-    return M, C, K, func_nl, func_nl_freq
+    return System(M, C, K, dMdU, dCdU, dKdU, fnlt, dfntldU, fnlf, dfnlfdU)
 
 # TODO: improve this disgusting function
 def tangent_predictor(J, zref, Xref):
@@ -148,6 +177,27 @@ def numerical_jac(f, x):
         jac[:, j] = (f(xp) - f(xm)) / delta
         if np.allclose(jac[:, j], np.zeros(n)):
             print("Jacobian zero problem")
+    
+    print("Condition number: ", np.linalg.cond(jac))
+    return jac
+
+def numerical_jac2(f, x, m, n):
+    """
+    Numerical Jacobian using precise central differences
+    Performs 2*N evaluations of the function
+    """
+    assert n == len(x)
+    assert m == len(f(x))
+
+    jac = np.zeros((m, n))
+    for j in range(n):
+        h = max(fd.cd2_h(x[j]), 1e-5) # limiter because hbvlm is in single precision
+        # print(f"{j}| h: {h:.5e}")
+        xp, xm = x.copy(), x.copy()
+        xp[j] += h
+        xm[j] -= h
+        delta = xp[j] - xm[j] # delta representable fp number
+        jac[:, j] = (f(xp) - f(xm)) / delta
     
     print("Condition number: ", np.linalg.cond(jac))
     return jac
@@ -213,6 +263,57 @@ def extended_residual(
 def extended_residual_jacobian(X, X_ref, z_ref, residual_func, parametrisation, *args):
     return numerical_jac(lambda _X: extended_residual(_X, X_ref, z_ref, residual_func, parametrisation, True, False), X)
 
+def nabla(H):
+    nabla = np.zeros((2*H+1, 2*H+1))
+    nabla_j = np.array([[0, 1], [-1, 0]])
+    for j in range(1, H+1):
+        nabla[2*j-1:2*j+1, 2*j-1:2*j+1] = j*nabla_j
+    return nabla
+
+def hb_jacobian(X, *args):
+    Om = X[-1]
+    param = X[-2]
+    sys = create_motion_system(Om, param, *args)
+    nab = nabla(H)
+
+    J = np.zeros((X.shape[0]-2, X.shape[0]))
+
+    J[:, :-2] = np.kron(Om**2 * nab @ nab, sys.M) + np.kron(Om * nab, sys.C) + np.kron(np.eye(2*H+1), sys.K)
+    J[:, -2] = (np.kron(Om**2 * nab @ nab, sys.dMdU) + np.kron(Om * nab, sys.dCdU) + np.kron(np.eye(2*H+1), sys.dKdU)) @ X[:-2]
+    J[:, -1] = (np.kron(2*Om * nab @ nab, sys.M) + np.kron(nab, sys.C)) @ X[:-2]
+    
+    nl_jacobian = jax.jacobian(hb_residual2)
+    J2 = nl_jacobian(X, *args)
+    assert J2.shape == J.shape, f"J2 shape: {J2.shape}, J shape: {J.shape}"
+    # J2 = numerical_jac2(hb_residual2, X, X.shape[0]-2, X.shape[0])
+    J += J2
+    return J
+
+def extended_residual_jacobian2(X, X_ref, z_ref, residual_func, parametrisation, *args):
+    Jext = np.zeros((X.shape[0], X.shape[0]))
+    Jext[:-2, :] = hb_jacobian(X, *args)
+    
+    X_mat_ref = X_ref[:-2].reshape(2*H+1, n_dofs).T
+    for k in range(1, H + 1):
+        # Calculate indices for the vectorized X elements
+        col_2k_minus_1 = int((2*k - 1) * n_dofs)
+        col_2k = int(2*k * n_dofs)
+        
+        # Term 1: k * X_ref[:, 2k] for odd columns
+        Jext[-2, col_2k_minus_1:col_2k_minus_1+n_dofs] = k * X_mat_ref[:, 2*k]
+        
+        # Term 2: -k * X_ref[:, 2k-1] for even columns
+        Jext[-2, col_2k:col_2k + n_dofs] = - k * X_mat_ref[:, 2*k - 1]
+    
+    Jext[-2, -2:] = 0.0 # OIC doesnt take into account param or omega
+    match parametrisation:
+        case Parametrisation.Local:
+            Jext[-1, :] = z_ref
+        case Parametrisation.ArcLength:
+            Jext[-1, :] = 2 * (X - X_ref)
+    
+    return Jext
+
 def hb_residual(X, *args):
     """
     X[:-2]: dof*(2*H+1) Fourier coefficients of the system [X0, Xc1, Xs1, ... XcH, XsH]
@@ -222,31 +323,34 @@ def hb_residual(X, *args):
     """
     Om = X[-1]
     param = X[-2]
-    M, C, K, func_nl, func_nl_freq = create_motion_system(Om, param, *args)
+    sys = create_motion_system(Om, param, *args)
     R_lin = np.zeros(X.shape[0]-2)
 
-    # Compute the linear forces in Fourier domain
-    R_lin[0:n_dofs] = K @ X[0:n_dofs]
-    for k in range(1, H+1):
-        i = (2*k-1) * n_dofs
-        R_lin[i:i+n_dofs] = (K - (k*Om)**2 * M) @ X[i:i+n_dofs] + k * Om * C @ X[i+n_dofs:i+2*n_dofs]
-        R_lin[i+n_dofs:i+2*n_dofs] = - k * Om * C @ X[i:i+n_dofs] + (K - (k*Om)**2 * M) @ X[i+n_dofs:i+2*n_dofs]
+    # Compute the linear forces in Fourier domain (kroenecker free formula)
+    # R_lin[0:n_dofs] = K @ X[0:n_dofs]
+    # for k in range(1, H+1):
+    #     i = (2*k-1) * n_dofs
+    #     R_lin[i:i+n_dofs] = (K - (k*Om)**2 * M) @ X[i:i+n_dofs] + k * Om * C @ X[i+n_dofs:i+2*n_dofs]
+    #     R_lin[i+n_dofs:i+2*n_dofs] = - k * Om * C @ X[i:i+n_dofs] + (K - (k*Om)**2 * M) @ X[i+n_dofs:i+2*n_dofs]
+
+    nab = nabla(H)
+    Z = np.kron(Om**2 * nab @ nab, sys.M) + np.kron(Om * nab, sys.C) + np.kron(np.eye(2*H+1), sys.K)
+    R_lin = Z @ X[:-2]
 
     # Optimized FFT version of AFT
     T = 2 * np.pi / Om      # period
     dt = T / n_samples              # time step
     Xc_real = X[:-2].reshape(n_coeffs, n_dofs).T
     Xc = vdp.X_to_complex(Xc_real) # Each row for each dof, each col corresponds to the jth fourier coeffs (a0, a1, b1, ... aH, bH)
-    q = np.fft.irfft(Xc, n_samples, axis=1, norm='forward') # no scaling
-
     k = np.arange(H+1)
+    q = np.fft.irfft(Xc, n_samples, axis=1, norm='forward') # no scaling
     q_dot = np.fft.irfft(1j * Om * k * Xc, n_samples, axis=1, norm='forward')
     # q_ddot = np.fft.ifft(- (w**2) * Q_fft).real
     R_nlt = np.zeros((n_dofs, n_samples))
     for s in range(n_samples):
         t_n = s * dt
-        R_nlt[:, s] = - func_nl(t_n, q[:, s], q_dot[:, s])
-    R_nlft = - func_nl_freq(Xc_real)
+        R_nlt[:, s] = - sys.fnlt(t_n, q[:, s], q_dot[:, s])
+    R_nlft = - sys.fnlf(Xc_real)
     
     R_nl_fft = np.fft.rfft(R_nlt, n_samples, axis=1, norm='backward') # no scaling
     R_nlf_fft = np.fft.rfft(R_nlft, n_coeffs, axis=1, norm='backward') # no scaling
@@ -257,35 +361,35 @@ def hb_residual(X, *args):
 
 @helpers.Timing(prefix="Solver:")
 def solve_nonlinear_system(X0, X_ref, z_ref, parametrisation, xtol=1e-5):
-    # Xp, info, ier, mesg =  sp.optimize.fsolve(
-    #     extended_residual,
-    #     X0,
-    #     args=(X_ref, z_ref, hb_residual, init, True, True),
-    #     fprime=extended_residual_jacobian,
-    #     full_output=True,
-    #     xtol = xtol,
-    # )
-    # if ier != 1:
-    #     print(f"Nonlinear solver failed: {mesg}")
-    #     exit(1)
-
-    # print(f"param: {Xp[-2]:.3f}, omega: {Xp[-1]:.3f}, nfev: {info['nfev']}, njev: {info['njev']}")
-
-    # return Xp
-
-    sol = sp.optimize.root(
+    Xp, info, ier, mesg =  sp.optimize.fsolve(
         extended_residual,
         X0,
         args=(X_ref, z_ref, hb_residual, parametrisation, True, True),
-        method='hybr',
-        jac=extended_residual_jacobian,
-        tol = xtol
+        fprime=extended_residual_jacobian,
+        full_output=True,
+        xtol = xtol,
     )
-    if not sol.success:
-        print(f"Nonlinear solver failed: {sol.message}")
+    if ier != 1:
+        print(f"Nonlinear solver failed: {mesg}")
         exit(1)
 
-    return sol.x
+    print(f"param: {Xp[-2]:.3f}, omega: {Xp[-1]:.3f}, nfev: {info['nfev']}, njev: {info['njev']}")
+
+    return Xp
+
+    # sol = sp.optimize.root(
+    #     extended_residual,
+    #     X0,
+    #     args=(X_ref, z_ref, hb_residual, parametrisation, True, True),
+    #     method='hybr',
+    #     jac=extended_residual_jacobian,
+    #     tol = xtol
+    # )
+    # if not sol.success:
+    #     print(f"Nonlinear solver failed: {sol.message}")
+    #     exit(1)
+
+    # return sol.x
 
 def continuation(param_start, param_end, ds, X0):
     """
@@ -410,12 +514,12 @@ if __name__ == "__main__":
         torsional_func = dof2.alpha_linear
 
     # Independent params
-    H = 15
+    H = 3
     n_dofs = 2
     n_coeffs = 2*H+1
     n_samples = (H+1)*(2**4) # sampling points (needs to be power of 2)
     flutter_speed = 6.285
-    flutter_ratio_start = 0.6
+    flutter_ratio_start = 0.3
     flutter_ratio_end = 0.8
     ds = 0.05
 
@@ -457,65 +561,65 @@ if __name__ == "__main__":
 
     # Initialize the gamma values
     run_hbvlm(0, 0, False, False, reset_logfile=True) # only reset logfile
-    _, _, _, _, hbvlm_func = create_motion_system(omega0, param_start, False, True) 
-    _ = hbvlm_func(X0[:-2].reshape(n_coeffs, n_dofs).T)
 
-    if (getenv("PLOT")):
-        fig2 = make_subplots(
-            rows=n_dofs, cols=1,
-            subplot_titles=[f"Dof {i+1}" for i in range(n_dofs)],
-        )
-        fig2.update_layout(title="2 DOF Force Response")
-        _, _, _, _, hbvlm_func = create_motion_system(omega0, param_start, True, False)
-        R_nlft = hbvlm_func(X0[:-2].reshape(n_coeffs, n_dofs).T)
-        R_nlf_fft = np.fft.rfft(R_nlft, n_coeffs, axis=1, norm='backward')
-        R_nlf = vdp.X_to_real(R_nlf_fft / n_coeffs, 0).T.reshape(-1)
-        plot_hb_timedomain(fig2, t_tr[0], t_tr[-1], 0.1, n_dofs, R_nlf, omega0, H, "HB")
-        fig2.update_yaxes(tickformat='.2e')
-        fig2.show()
-
-        fig = make_subplots(
-            rows=n_dofs, cols=1,
-            subplot_titles=[f"Dof {i+1}" for i in range(n_dofs)],
-            vertical_spacing=0.1,
-            horizontal_spacing=0.08
-        )
-        fig.update_layout(title="2 DOF Aeroelastic Response")
-        plot_hb_timedomain(fig, t_tr[0], t_tr[-1], 0.1, n_dofs, X0[:-2], X0[-1], H, "FFT")
-        
-        z_ref = np.zeros_like(X0)
-        z_ref[-2] = 1
-        Xpp = solve_nonlinear_system(X0, X0.copy(), z_ref, True)
-        plot_hb_timedomain(fig, t_tr[0], t_tr[-1], 0.1, n_dofs, Xpp[:-2], Xpp[-1], H, "HB")
-
-        for dof in range(n_dofs):
-            fig.add_trace(
-                go.Scatter(
-                    x=t_tr,  # x values for new line
-                    y=u_tr[dof, :],  # y values for new line
-                    name=f"Time integration dof {dof}",  # legend label
-                    line=dict(color='red')  # optional: customize line color
-                ),
-                row=(dof+1),
-                col=1
-            )
-        fig.show()
-
-    X_mat = continuation(param_start, param_end, ds, X0)
+    sys = create_motion_system(omega0, param_start, False, True) 
+    R_nlft = sys.fnlf(X0[:-2].reshape(n_coeffs, n_dofs).T)
     
-    if getenv("PLOT"):
-        fig = go.Figure()
-        fig.update_layout(title=f"2DOF Aeroelasticd Response ({torsional_spring_names[torsional_spring]} Pitch)")
-        for h in range(1, H+1):
-            fig.add_trace(
-                go.Scattergl(
-                    x = X_mat[-2, :],
-                    y = np.sqrt(X_mat[2*h-1, :]**2 + X_mat[2*h, :]**2),
-                    name = f"Harmonic {h}",
-                    mode = "lines+markers"
-                )
-            )
-        fig.update_xaxes(title_text=r"$\bar{U}$", showgrid=True)
-        fig.update_yaxes(title_text=r"$||H_{j}||^{2}$", showgrid=True)
-        fig.write_html("build/continuation.html", include_mathjax='cdn')
-        fig.show()
+    # if (getenv("PLOT")):
+        # fig2 = make_subplots(
+        #     rows=n_dofs, cols=1,
+        #     subplot_titles=[f"Dof {i+1}" for i in range(n_dofs)],
+        # )
+        # fig2.update_layout(title="2 DOF Force Response")
+        
+        # R_nlf_fft = np.fft.rfft(R_nlft, n_coeffs, axis=1, norm='backward')
+        # R_nlf = vdp.X_to_real(R_nlf_fft / n_coeffs, 0).T.reshape(-1)
+        # plot_hb_timedomain(fig2, t_tr[0], t_tr[-1], 0.1, n_dofs, R_nlf, omega0, H, "HB")
+        # fig2.update_yaxes(tickformat='.2e')
+        # fig2.show()
+
+        # fig = make_subplots(
+        #     rows=n_dofs, cols=1,
+        #     subplot_titles=[f"Dof {i+1}" for i in range(n_dofs)],
+        #     vertical_spacing=0.1,
+        #     horizontal_spacing=0.08
+        # )
+        # fig.update_layout(title="2 DOF Aeroelastic Response")
+        # plot_hb_timedomain(fig, t_tr[0], t_tr[-1], 0.1, n_dofs, X0[:-2], X0[-1], H, "FFT")
+        
+        # z_ref = np.zeros_like(X0)
+        # z_ref[-2] = 1
+        # Xpp = solve_nonlinear_system(X0, X0.copy(), z_ref, True)
+        # plot_hb_timedomain(fig, t_tr[0], t_tr[-1], 0.1, n_dofs, Xpp[:-2], Xpp[-1], H, "HB")
+
+        # for dof in range(n_dofs):
+        #     fig.add_trace(
+        #         go.Scatter(
+        #             x=t_tr,  # x values for new line
+        #             y=u_tr[dof, :],  # y values for new line
+        #             name=f"Time integration dof {dof}",  # legend label
+        #             line=dict(color='red')  # optional: customize line color
+        #         ),
+        #         row=(dof+1),
+        #         col=1
+        #     )
+        # fig.show()
+
+    # X_mat = continuation(param_start, param_end, ds, X0)
+    
+    # if getenv("PLOT"):
+    #     fig = go.Figure()
+    #     fig.update_layout(title=f"2DOF Aeroelasticd Response ({torsional_spring_names[torsional_spring]} Pitch)")
+    #     for h in range(1, H+1):
+    #         fig.add_trace(
+    #             go.Scattergl(
+    #                 x = X_mat[-2, :],
+    #                 y = np.sqrt(X_mat[2*h-1, :]**2 + X_mat[2*h, :]**2),
+    #                 name = f"Harmonic {h}",
+    #                 mode = "lines+markers"
+    #             )
+    #         )
+    #     fig.update_xaxes(title_text=r"$\bar{U}$", showgrid=True)
+    #     fig.update_yaxes(title_text=r"$||H_{j}||^{2}$", showgrid=True)
+    #     fig.write_html("build/continuation.html", include_mathjax='cdn')
+    #     fig.show()
